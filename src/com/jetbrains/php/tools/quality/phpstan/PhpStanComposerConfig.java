f83b23b9bb7e3232b5aaab75e86aa7bac2ad1fca
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2019-01-30 17:49:23 +0100

    phpstan: composer config to easy checks the projects with composer.json
    GitOrigin-RevId: 3ca943007af6fe7e760a6ee4815813b762b6fa09

---
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanComposerConfig.java
@@ -0,0 +1,47 @@
+package com.jetbrains.php.tools.quality.phpstan;
+
+import com.intellij.openapi.project.Project;
+import com.jetbrains.php.composer.actions.log.ComposerLogMessageBuilder;
+import com.jetbrains.php.config.interpreters.PhpInterpreter;
+import com.jetbrains.php.tools.quality.QualityToolConfigurationManager;
+import com.jetbrains.php.tools.quality.QualityToolsComposerConfig;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class PhpStanComposerConfig extends QualityToolsComposerConfig<PhpStanConfiguration, PhpStanValidationInspection> {
+  private static final String PACKAGE = "phpstan/phpstan";
+  private static final String RELATIVE_PATH = "phpstan/phpstan/bin/phpstan";
+  private static final String PHPSTAN_DISPLAY_NAME = "PHPStan";
+  private static final PhpStanValidationInspection PHP_STAN_VALIDATION_INSPECTION = new PhpStanValidationInspection();
+
+
+  public PhpStanComposerConfig() {
+    super(PACKAGE, RELATIVE_PATH);
+  }
+
+  @Override
+  protected ComposerLogMessageBuilder.Settings getQualityToolsInspectionSettings() {
+    return null;
+  }
+
+  @Override
+  protected PhpStanValidationInspection getQualityInspection() {
+    return PHP_STAN_VALIDATION_INSPECTION;
+  }
+
+  @Override
+  protected ComposerLogMessageBuilder.Settings getQualityToolsSettings() {
+    return null;
+  }
+
+  @Override
+  protected boolean applyRulesetFromComposer(@NotNull Project project, PhpStanConfiguration configuration) {
+    return true;
+  }
+
+  @NotNull
+  @Override
+  protected QualityToolConfigurationManager<PhpStanConfiguration> getConfigurationManager(@NotNull Project project) {
+    return PhpStanConfigurationManager.getInstance(project);
+  }
+}
