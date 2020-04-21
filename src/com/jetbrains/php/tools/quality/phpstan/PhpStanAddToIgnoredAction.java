56d47078c65ad14a8672849ee2835bac56c57cf9
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2020-04-21 16:40:56 +0200

    quality tools: remove not needed methods
    GitOrigin-RevId: 9d2e0a53253fbc43806c24ee4ddf5cecd4569fe5

--- src/com/jetbrains/php/tools/quality/phpstan/PhpStanAddToIgnoredAction.java
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanAddToIgnoredAction.java
@@ -3,31 +3,15 @@ package com.jetbrains.php.tools.quality.phpstan;
 import com.intellij.openapi.project.Project;
 import com.jetbrains.php.tools.quality.QualityToolAddToIgnoredAction;
 import com.jetbrains.php.tools.quality.QualityToolBlackList;
-import com.jetbrains.php.tools.quality.QualityToolProjectConfigurableForm;
 import com.jetbrains.php.tools.quality.QualityToolsIgnoreFilesConfigurable;
 import org.jetbrains.annotations.NotNull;
 
-import static com.jetbrains.php.tools.quality.phpstan.PhpStanConfigurationBaseManager.PHP_STAN;
-
 public class PhpStanAddToIgnoredAction extends QualityToolAddToIgnoredAction {
-
 
   @NotNull
   @Override
   protected QualityToolBlackList getBlackList(Project project) {
     return PhpStanBlackList.getInstance(project);
-  }
-
-  @NotNull
-  @Override
-  protected String getToolName() {
-    return PHP_STAN;
-  }
-
-  @NotNull
-  @Override
-  protected QualityToolProjectConfigurableForm getToolConfigurable(@NotNull Project project) {
-    return new PhpStanConfigurable(project);
   }
 
   @Override
