10186c2102c39eb8f55f2293501d46946fc12c9d
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2020-04-21 15:27:05 +0200

    quality tools: remove unused method
    GitOrigin-RevId: 7b8429fcb0aca8911966a8e015911bf5ec2e4935

--- src/com/jetbrains/php/tools/quality/phpstan/PhpStanAnnotatorProxy.java
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanAnnotatorProxy.java
@@ -1,43 +1,18 @@
 package com.jetbrains.php.tools.quality.phpstan;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
 import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.execution.ExecutionException;
-import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.execution.process.OSProcessHandler;
-import com.intellij.execution.process.ProcessAdapter;
-import com.intellij.execution.process.ProcessEvent;
-import com.intellij.execution.process.ProcessOutputTypes;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ProjectRootManager;
-import com.intellij.openapi.util.Key;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
-import com.intellij.util.SmartList;
 import com.jetbrains.php.config.interpreters.PhpSdkFileTransfer;
 import com.jetbrains.php.tools.quality.*;
-import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.StringReader;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.intellij.openapi.util.text.StringUtil.equalsIgnoreWhitespaces;
-import static com.intellij.util.containers.ContainerUtil.newArrayList;
 import static com.jetbrains.php.tools.quality.QualityToolProcessCreator.runToolProcess;
 
 public class PhpStanAnnotatorProxy extends QualityToolAnnotator {
   public final static PhpStanAnnotatorProxy INSTANCE = new PhpStanAnnotatorProxy();
-  private static final Logger LOG = Logger.getInstance(PhpStanAnnotatorProxy.class);
-  @NonNls private static final String TEMP_DIRECTORY = "PhpStan_temp.tmp";
 
   @Nullable
   @Override
@@ -49,12 +24,6 @@ public class PhpStanAnnotatorProxy extends QualityToolAnnotator {
       // skip
     }
     return null;
-  }
-
-  @NotNull
-  @Override
-  protected String getTemporaryFilesFolder() {
-    return TEMP_DIRECTORY;
   }
 
   @NotNull
