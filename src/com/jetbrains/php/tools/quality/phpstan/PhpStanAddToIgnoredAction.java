d92422afaf63f7caff687237a6bd57e9f8ff1b3e
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2020-04-23 17:17:47 +0200

    quality tools: remove *IgnoreFilesConfigurable
    GitOrigin-RevId: 9ba0f1deac26cdf5dc62ba7d7672b7039f5f1f94

--- src/com/jetbrains/php/tools/quality/phpstan/PhpStanAddToIgnoredAction.java
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanAddToIgnoredAction.java
@@ -3,7 +3,6 @@ package com.jetbrains.php.tools.quality.phpstan;
 import com.intellij.openapi.project.Project;
 import com.jetbrains.php.tools.quality.QualityToolAddToIgnoredAction;
 import com.jetbrains.php.tools.quality.QualityToolBlackList;
-import com.jetbrains.php.tools.quality.QualityToolsIgnoreFilesConfigurable;
 import org.jetbrains.annotations.NotNull;
 
 public class PhpStanAddToIgnoredAction extends QualityToolAddToIgnoredAction {
@@ -12,10 +11,5 @@ public class PhpStanAddToIgnoredAction extends QualityToolAddToIgnoredAction {
   @Override
   protected QualityToolBlackList getBlackList(Project project) {
     return PhpStanBlackList.getInstance(project);
-  }
-
-  @Override
-  protected QualityToolsIgnoreFilesConfigurable getIgnoredFilesConfigurable(@NotNull Project project) {
-    return new PhpStanIgnoredFilesConfigurable(project);
   }
 }
--- src/com/jetbrains/php/tools/quality/phpstan/PhpStanBlackList.java
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanBlackList.java
@@ -6,11 +6,21 @@ import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.components.StoragePathMacros;
 import com.intellij.openapi.project.Project;
 import com.jetbrains.php.tools.quality.QualityToolBlackList;
+import org.jetbrains.annotations.NotNull;
+
+import static com.jetbrains.php.tools.quality.phpstan.PhpStanConfigurationBaseManager.PHP_STAN;
 
 @State(name = "PhpStanBlackList", storages = {@Storage(StoragePathMacros.WORKSPACE_FILE)})
 public class PhpStanBlackList extends QualityToolBlackList {
 
   public static PhpStanBlackList getInstance(Project project) {
     return ServiceManager.getService(project, PhpStanBlackList.class);
+  }
+
+
+  @NotNull
+  @Override
+  public String getToolName() {
+    return PHP_STAN;
   }
 }
