a6b838e41624d6a37108ca1ea827cce05ab8ac90
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2020-04-24 13:51:15 +0200

    quality tools: eliminate *ConfigurationCombobox
    GitOrigin-RevId: b96aac83e2f6bc22ebe858f1863281d8c1903137

--- src/com/jetbrains/php/tools/quality/phpstan/PhpStanConfigurable.java
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanConfigurable.java
@@ -2,7 +2,6 @@ package com.jetbrains.php.tools.quality.phpstan;
 
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.project.Project;
-import com.jetbrains.php.tools.quality.QualityToolConfigurationComboBox;
 import com.jetbrains.php.tools.quality.QualityToolProjectConfigurableForm;
 import com.jetbrains.php.tools.quality.QualityToolProjectConfiguration;
 import com.jetbrains.php.tools.quality.QualityToolType;
@@ -41,12 +40,6 @@ public class PhpStanConfigurable extends QualityToolProjectConfigurableForm impl
   @Override
   protected String getInspectionShortName() {
     return new PhpStanValidationInspection().getShortName();
-  }
-
-  @NotNull
-  @Override
-  protected QualityToolConfigurationComboBox createConfigurationComboBox() {
-    return new PhpStanConfigurationComboBox(myProject);
   }
 
   @Override
--- src/com/jetbrains/php/tools/quality/phpstan/PhpStanQualityToolType.java
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanQualityToolType.java
@@ -2,12 +2,15 @@ package com.jetbrains.php.tools.quality.phpstan;
 
 import com.intellij.openapi.project.Project;
 import com.jetbrains.php.tools.quality.QualityToolBlackList;
+import com.jetbrains.php.tools.quality.QualityToolConfigurableList;
+import com.jetbrains.php.tools.quality.QualityToolConfigurationManager;
 import com.jetbrains.php.tools.quality.QualityToolType;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import static com.jetbrains.php.tools.quality.phpstan.PhpStanConfigurationBaseManager.PHP_STAN;
 
-public class PhpStanQualityToolType extends QualityToolType {
+public class PhpStanQualityToolType extends QualityToolType<PhpStanConfiguration> {
   @NotNull
   @Override
   public String getDisplayName() {
@@ -17,5 +20,16 @@ public class PhpStanQualityToolType extends QualityToolType {
   @Override
   public @NotNull QualityToolBlackList getQualityToolBlackList(@NotNull Project project) {
     return PhpStanBlackList.getInstance(project);
+  }
+
+  @Override
+  protected QualityToolConfigurableList<PhpStanConfiguration> getQualityToolConfigurableList(@NotNull Project project,
+                                                                                             @Nullable String item) {
+    return new PhpStanConfigurableList(project, item);
+  }
+
+  @Override
+  protected QualityToolConfigurationManager<PhpStanConfiguration> getConfigurationManager(@NotNull Project project) {
+    return PhpStanConfigurationManager.getInstance(project);
   }
 }
