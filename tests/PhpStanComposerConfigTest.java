e44011f5efbd41f2a6c69c3af8622d1292905268
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2020-05-08 18:24:01 +0200

    phpstan/psalm: basic tests
    GitOrigin-RevId: 95c6d3aa25e3c541754a52f47856a4d1592ca5c5

---
+++ tests/PhpStanComposerConfigTest.java
@@ -0,0 +1,25 @@
+import com.intellij.openapi.util.SystemInfo;
+import com.jetbrains.php.config.composer.QualityToolsComposerConfigTest;
+import com.jetbrains.php.tools.quality.QualityToolConfigurationManager;
+import com.jetbrains.php.tools.quality.phpstan.PhpStanConfigurationManager;
+import org.jetbrains.annotations.NotNull;
+
+public class PhpStanComposerConfigTest extends QualityToolsComposerConfigTest {
+
+  @Override
+  protected QualityToolConfigurationManager getQualityToolConfigurationManager() {
+    return PhpStanConfigurationManager.getInstance(getProject());
+  }
+
+  @NotNull
+  @Override
+  protected String getPath() {
+    return "phpstan/phpstan/bin/phpstan" + (SystemInfo.isWindows ? ".bat" : "");
+  }
+
+  @NotNull
+  @Override
+  protected String getPackageName() {
+    return "phpstan/phpstan";
+  }
+}
