047ac1a7d6bfbed44851d0a548cf228edb22ad81
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2020-07-07 15:43:42 +0200

    WI-54171 phpstan: detect config from composer.json - tests
    GitOrigin-RevId: afc36b6ae8cc28311b2e1c5b78e711fa915d60a1

---
+++ tests/PhpStanConfigFileFromComposerTest.groovy
@@ -0,0 +1,79 @@
+
+
+import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase
+import com.jetbrains.php.tools.quality.phpstan.PhpStanComposerConfig
+import org.jetbrains.annotations.NotNull
+import org.jetbrains.annotations.Nullable
+
+class PhpStanConfigFileFromComposerTest extends CodeInsightFixtureTestCase {
+
+  @NotNull
+  private void doRulesetTest(@NotNull String content, @Nullable String expectedPath) {
+    myFixture.configureByText("composer.json", content)
+    try {
+      def configFile = myFixture.getFile().getVirtualFile()
+      assertEquals(expectedPath, new PhpStanComposerConfig().getRuleset(configFile))
+    }
+    finally {
+    }
+  }
+
+  void testCFormat() {
+    doRulesetTest('''
+{
+    "scripts": {
+        "phpstan": "vendor/bin/phpstan analyse -l 2 -c tests/phpstan/phpstan.neon system/src --memory-limit=256M"
+    }
+}
+''', "tests/phpstan/phpstan.neon")
+  }
+
+  void testNoScriptsSection() {
+    doRulesetTest('''
+{
+}
+''', null)
+  }
+
+  void testNoPsalmInScriptsSection() {
+    doRulesetTest('''
+{
+    "scripts": {
+    }
+}
+''', null)
+  }
+  
+
+  void testConfigFormat() {
+    doRulesetTest('''
+{
+  "scripts": {
+        "phpstan": "vendor/bin/phpstan analyse -l 2 --configuration=tests/phpstan/phpstan.neon system/src --memory-limit=256M"
+  }
+}
+''', "tests/phpstan/phpstan.neon")
+  }
+
+  void testNoConfigArg() {
+    doRulesetTest('''
+{
+  "scripts": {
+        "phpstan": "vendor/bin/phpstan analyse -l 2 system/src --memory-limit=256M"
+  }
+}
+''', null)
+  }
+
+
+  void testWrongFormat() {
+    doRulesetTest('''
+{
+"scripts": {
+        "phpstan": "vendor/bin/phpstan analyse -l 2 -c"
+  }
+}
+''', null)
+  }
+
+}
