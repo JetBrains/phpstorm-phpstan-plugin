3c653b6e8b875d2e62d4100bd2fa8c1a51b4f612
dbrekelmans <dabrekelmans@gmail.com>
2020-07-10 14:26:58 +0200

    Replace psalm with phpstan

--- tests/com/jetbrains/php/psalm/quality/tools/PhpStanComposerConfigTest.java
+++ tests/com/jetbrains/php/psalm/quality/tools/PhpStanComposerConfigTest.java
@@ -1,4 +1,4 @@
-package com.jetbrains.php.psalm.quality.tools;
+package com.jetbrains.php.phpstan.quality.tools;
 
 import com.intellij.openapi.util.SystemInfo;
 import com.jetbrains.php.config.composer.QualityToolsComposerConfigTest;
