e0174ca05bb5b3f186f427ea2a07c43c5c70286f
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2020-07-17 14:44:54 +0700

    Annotate bundle methods as Nls
    GitOrigin-RevId: db851b498d99f538e1f747faa4fb4d954848982f

--- src/com/jetbrains/php/tools/quality/phpstan/PhpStanBundle.java
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanBundle.java
@@ -1,6 +1,7 @@
 package com.jetbrains.php.tools.quality.phpstan;
 
 import com.intellij.DynamicBundle;
+import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
@@ -14,12 +15,12 @@ public final class PhpStanBundle extends DynamicBundle {
   private PhpStanBundle() { super(BUNDLE); }
 
   @NotNull
-  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
+  public static @Nls String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
     return INSTANCE.getMessage(key, params);
   }
 
   @NotNull
-  public static Supplier<String> messagePointer(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
+  public static Supplier<@Nls String> messagePointer(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
     return INSTANCE.getLazyMessage(key, params);
   }
 }
