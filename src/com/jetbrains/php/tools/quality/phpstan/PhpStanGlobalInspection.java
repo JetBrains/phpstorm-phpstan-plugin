7f041fd0056db8226b041c7540f9e9ccbdffdaae
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2020-07-23 18:29:56 +0200

    WI-54189 Psalm/PhpStan quality tools performance - re-use output of phpstan in batch inspection
    GitOrigin-RevId: 5a69b7142a3b893d1868a5294cdbc5fdd12d5464

--- src/com/jetbrains/php/tools/quality/phpstan/PhpStanGlobalInspection.java
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanGlobalInspection.java
@@ -1,13 +1,26 @@
 package com.jetbrains.php.tools.quality.phpstan;
 
-import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.codeInspection.*;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.util.Key;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.containers.ContainerUtil;
 import com.jetbrains.php.tools.quality.QualityToolAnnotator;
+import com.jetbrains.php.tools.quality.QualityToolMessage;
+import com.jetbrains.php.tools.quality.QualityToolMessageProcessor;
 import com.jetbrains.php.tools.quality.QualityToolValidationGlobalInspection;
+import com.jetbrains.php.tools.quality.phpstan.PhpStanMessageProcessor.PhpStanXmlMessageHandler.ProblemDescription;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class PhpStanGlobalInspection extends QualityToolValidationGlobalInspection {
+import java.util.List;
 
+import static com.intellij.codeInspection.ProblemHighlightType.WEAK_WARNING;
+
+public class PhpStanGlobalInspection extends QualityToolValidationGlobalInspection {
+  public static final Key<List<ProblemDescription>> PHPSTAN_ANNOTATOR_INFO = Key.create("ANNOTATOR_INFO_2");
 
   @Override
   public @Nullable LocalInspectionTool getSharedLocalInspectionTool() {
@@ -17,5 +30,35 @@ public class PhpStanGlobalInspection extends QualityToolValidationGlobalInspecti
   @Override
   protected @NotNull QualityToolAnnotator getAnnotator() {
     return PhpStanAnnotatorProxy.INSTANCE;
+  }
+
+  @Override
+  public void checkFile(@NotNull PsiFile file,
+                        @NotNull InspectionManager manager,
+                        @NotNull ProblemsHolder problemsHolder,
+                        @NotNull GlobalInspectionContext globalContext,
+                        @NotNull ProblemDescriptionsProcessor problemDescriptionsProcessor) {
+    final List<ProblemDescription> data = globalContext.getProject().getUserData(PHPSTAN_ANNOTATOR_INFO);
+    if (data == null) {
+      return;
+    }
+
+    final List<ProblemDescription> fileData = ContainerUtil.filter(data, i -> file.getVirtualFile().getPath().endsWith(i.getFile()));
+    for (PhpStanMessageProcessor.PhpStanXmlMessageHandler.ProblemDescription problem : fileData) {
+      final Document document = PsiDocumentManager.getInstance(file.getProject()).getDocument(file);
+      QualityToolMessage qualityToolMessage;
+      if (document != null && problem.getLineNumber() - 1 > 0 && problem.getLineNumber() - 1 < document.getLineCount()) {
+        qualityToolMessage = new QualityToolMessage(file, TextRange
+          .create(document.getLineStartOffset(problem.getLineNumber() - 1) + problem.getColumn(),
+                  document.getLineEndOffset(problem.getLineNumber() - 1)), problem.getSeverity(), problem.getMessage());
+      }
+      else {
+        qualityToolMessage = new QualityToolMessage(file, problem.getLineNumber(), problem.getSeverity(), problem.getMessage());
+      }
+      final QualityToolMessageProcessor messageProcessor = globalContext.getProject().getUserData(ANNOTATOR_INFO);
+      if (messageProcessor != null) {
+        problemsHolder.registerProblem(manager.createProblemDescriptor(file, qualityToolMessage.getTextRange(), qualityToolMessage.getMessageText(), WEAK_WARNING, false));
+      }
+    }
   }
 }
--- src/com/jetbrains/php/tools/quality/phpstan/PhpStanMessageProcessor.java
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanMessageProcessor.java
@@ -2,9 +2,8 @@ package com.jetbrains.php.tools.quality.phpstan;
 
 import com.intellij.codeHighlighting.HighlightDisplayLevel;
 import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.openapi.util.Trinity;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
 import com.jetbrains.php.tools.quality.QualityToolAnnotatorInfo;
@@ -26,6 +25,7 @@ import java.util.Set;
 
 import static com.jetbrains.php.tools.quality.QualityToolMessage.Severity.ERROR;
 import static com.jetbrains.php.tools.quality.QualityToolMessage.Severity.WARNING;
+import static com.jetbrains.php.tools.quality.phpstan.PhpStanGlobalInspection.PHPSTAN_ANNOTATOR_INFO;
 
 public class PhpStanMessageProcessor extends QualityToolXmlMessageProcessor {
   private final static String ERROR_TAG = "error";
@@ -38,16 +38,18 @@ public class PhpStanMessageProcessor extends QualityToolXmlMessageProcessor {
   private final static String MESSAGE_ATTR = "message";
   @NonNls private final static String SEVERITY_ATTR = "severity";
   private final static String FILE_NAME_ATTR = "name";
-  private final Set<Trinity<Pair<Integer, Integer>, String, QualityToolMessage.Severity>> lineMessages = new HashSet<>();
+  private final Set<PhpStanXmlMessageHandler.ProblemDescription> lineMessages = new HashSet<>();
   private final HighlightDisplayLevel myWarningsHighlightLevel;
   final String myFilePath;
   final PsiFile myPsiFile;
+  final Project myProject;
 
   protected PhpStanMessageProcessor(QualityToolAnnotatorInfo info) {
     super(info);
     myWarningsHighlightLevel = HighlightDisplayLevel.WARNING; // TODO: fix
     myFilePath = info.getOriginalFile() != null ? info.getOriginalFile().getPath(): null;
     myPsiFile = info.getPsiFile();
+    myProject = info.getProject();
   }
 
   @Override
@@ -55,20 +57,24 @@ public class PhpStanMessageProcessor extends QualityToolXmlMessageProcessor {
     PhpStanXmlMessageHandler messageHandler = (PhpStanXmlMessageHandler)getXmlMessageHandler(myFilePath);
     mySAXParser.parse(source, messageHandler);
     if (messageHandler.isStatusValid()) {
-      for (Trinity<Pair<Integer, Integer>, String, QualityToolMessage.Severity> problem : messageHandler.getProblemList()) {
-        final Document document = PsiDocumentManager.getInstance(myPsiFile.getProject()).getDocument(myPsiFile);
-        QualityToolMessage qualityToolMessage;
-        if (document != null && problem.first.first - 1 > 0 && problem.first.first - 1 < document.getLineCount()) {
-          qualityToolMessage = new QualityToolMessage(this, TextRange
-            .create(document.getLineStartOffset(problem.first.first - 1) + problem.first.second,
-                    document.getLineEndOffset(problem.first.first - 1)), problem.third, problem.second);
+      if (myPsiFile != null) {
+        for (PhpStanXmlMessageHandler.ProblemDescription problem : messageHandler.getProblemList()) {
+          final Document document = PsiDocumentManager.getInstance(myPsiFile.getProject()).getDocument(myPsiFile);
+          QualityToolMessage qualityToolMessage;
+          if (document != null && problem.getLineNumber() - 1 > 0 && problem.getLineNumber() - 1 < document.getLineCount()) {
+            qualityToolMessage = new QualityToolMessage(this, TextRange
+              .create(document.getLineStartOffset(problem.getLineNumber() - 1) + problem.getColumn(),
+                      document.getLineEndOffset(problem.getLineNumber() - 1)), problem.getSeverity(), problem.getMessage());
+          }
+          else {
+            qualityToolMessage = new QualityToolMessage(this, problem.getLineNumber(), problem.getSeverity(), problem.getMessage());
+          }
+          if (lineMessages.add(problem)) {
+            addMessage(qualityToolMessage);
+          }
         }
-        else {
-          qualityToolMessage = new QualityToolMessage(this, problem.first.first, problem.third, problem.second);
-        }
-        if (lineMessages.add(problem)) {
-          addMessage(qualityToolMessage);
-        }
+      } else {
+        myProject.putUserData(PHPSTAN_ANNOTATOR_INFO, messageHandler.getProblemList());
       }
     }
   }
@@ -115,32 +121,70 @@ public class PhpStanMessageProcessor extends QualityToolXmlMessageProcessor {
     return false;
   }
 
-  private static final class PhpStanXmlMessageHandler extends XMLMessageHandler {
+  static final class PhpStanXmlMessageHandler extends XMLMessageHandler {
 
     private final String myFilePath;
+    private String myFileAttr;
 
     private PhpStanXmlMessageHandler(@Nullable String filePath) {
       myFilePath = filePath;
     }
 
-    private List<Trinity<Pair<Integer, Integer>, String, QualityToolMessage.Severity>> myProblemList;
+    private List<ProblemDescription> myProblemList;
 
-    private List<Trinity<Pair<Integer, Integer>, String, QualityToolMessage.Severity>> getProblemList() {
+    private List<ProblemDescription> getProblemList() {
       return myProblemList;
     }
 
     @Override
     protected void parseTag(@NotNull String tagName, @NotNull Attributes attributes) {
       if (FILE_TAG.equals(tagName)) {
-        myProblemList = myFilePath == null || myFilePath.endsWith(attributes.getValue(FILE_NAME_ATTR)) ? new ArrayList<>() : null;
+        myFileAttr = attributes.getValue(FILE_NAME_ATTR);
+        myProblemList = myFilePath == null || myFilePath.endsWith(myFileAttr) ? new ArrayList<>() : null;
       }
       else if (ERROR_TAG.equals(tagName) | WARNING_TAG.equals(tagName)) {
         if (myProblemList != null) {
           mySeverity = attributes.getValue(SEVERITY_ATTR).equals(ERROR_TAG) ? ERROR : WARNING;
           myLineNumber = parseLineNumber(attributes.getValue(LINE_NUMBER_ATTR));
           int column = parseLineNumber(attributes.getValue(COLUMN_NUMBER_ATTR));
-          myProblemList.add(Trinity.create(Pair.create(myLineNumber, column), attributes.getValue(MESSAGE_ATTR), mySeverity));
+          myProblemList.add(new ProblemDescription(mySeverity, myLineNumber, column, attributes.getValue(MESSAGE_ATTR), myFileAttr));
         }
+      }
+    }
+    
+    static class ProblemDescription{
+      ProblemDescription(QualityToolMessage.Severity severity, int lineNumber, int column, String message, String file) {
+        mySeverity = severity;
+        myLineNumber = lineNumber;
+        myColumn = column;
+        myMessage = message;
+        myFile = file;
+      }
+
+      private final QualityToolMessage.Severity mySeverity;
+      private final int myLineNumber;
+      private final int myColumn;
+      private final String myMessage;
+      private final String myFile;
+
+      QualityToolMessage.Severity getSeverity() {
+        return mySeverity;
+      }
+
+      int getLineNumber() {
+        return myLineNumber;
+      }
+
+      int getColumn() {
+        return myColumn;
+      }
+
+      String getMessage() {
+        return myMessage;
+      }
+
+      String getFile() {
+        return myFile;
       }
     }
   }
