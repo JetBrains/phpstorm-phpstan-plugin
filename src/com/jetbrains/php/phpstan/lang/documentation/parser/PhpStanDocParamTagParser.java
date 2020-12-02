2603032c5fc3ca2154e72c8d4b90a2a0431226e3
Kirill Smelov <kirill.smelov@jetbrains.com>
2020-12-02 17:11:53 +0300

    WI-56572 Basic parsing for phpstan doc tags
    (cherry picked from commit bc1ea20f7700e804add35cbc32852165e2b03403)
    
    IJ-CR-4395
    
    GitOrigin-RevId: 83eebe9b8929bc9f227df179f8c69113bd4de324

---
+++ src/com/jetbrains/php/phpstan/lang/documentation/parser/PhpStanDocParamTagParser.java
@@ -0,0 +1,6 @@
+package com.jetbrains.php.phpstan.lang.documentation.parser;
+
+import com.jetbrains.php.lang.documentation.phpdoc.parser.tags.PhpDocParamTagParser;
+
+public class PhpStanDocParamTagParser extends PhpDocParamTagParser {
+}
---
+++ src/com/jetbrains/php/phpstan/lang/documentation/parser/PhpStanDocReturnTagParser.java
@@ -0,0 +1,6 @@
+package com.jetbrains.php.phpstan.lang.documentation.parser;
+
+import com.jetbrains.php.lang.documentation.phpdoc.parser.tags.PhpDocReturnTagParser;
+
+public class PhpStanDocReturnTagParser extends PhpDocReturnTagParser {
+}
