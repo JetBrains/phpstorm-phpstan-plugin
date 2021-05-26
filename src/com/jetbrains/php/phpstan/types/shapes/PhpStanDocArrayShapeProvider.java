3a3959dce880dd835a12ccb491d951cdcb3eac47
Kirill Smelov <kirill.smelov@jetbrains.com>
2021-05-26 18:14:22 +0300

    WI-56038 Support one-line version of phpdoc array shapes
    - Parsing
    - Type inference for array access expressions
    - Key completion array access expressions
    
    GitOrigin-RevId: 8a8a57143232300c6c4d6b2b76e36ec35ecf91a1

---
+++ src/com/jetbrains/php/phpstan/types/shapes/PhpStanDocArrayShapeProvider.java
@@ -0,0 +1,6 @@
+package com.jetbrains.php.phpstan.types.shapes;
+
+import com.jetbrains.php.lang.psi.resolve.types.shapes.PhpDocArrayShapeProvider;
+
+public class PhpStanDocArrayShapeProvider extends PhpDocArrayShapeProvider {
+}
