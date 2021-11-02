4d5e57ab6aafe59b5bf2f1c11f7bbfbf616e89e9
Alexey Kudravtsev <cdr@intellij.com>
2021-11-02 12:33:00 +0100

    When instantiating GlobalInspectionToolWrapper from GlobalInspectionTool, try to find its EP to retrieve more information (shortName/displayName/etc) from there, just like LocalInspectionTool does and to fix EA-400450 - PE: PluginManagerCore.createPluginException
    GitOrigin-RevId: d4f756f59d32408e7a4fa07c1246ac13f2fd3667

--- testData/output/Simple.php
+++ testData/output/Simple.php
@@ -5,35 +5,35 @@ final class SourceCode
 {
     const TYPE_XML = 'XML';
 
-<warning descr="phpstan: Property WebServCo\Framework\SourceCode::$type has no typehint specified.">   protected $type;</warning>
-<warning descr="phpstan: Property WebServCo\Framework\SourceCode::$data has no typehint specified.">   protected $data;</warning>
+<weak_warning descr="phpstan: Property WebServCo\Framework\SourceCode::$type has no typehint specified.">   protected $type;</weak_warning>
+<weak_warning descr="phpstan: Property WebServCo\Framework\SourceCode::$data has no typehint specified.">   protected $data;</weak_warning>
 
-<warning descr="phpstan: Method WebServCo\Framework\SourceCode::__construct() has parameter $data with no typehint specified."><warning descr="phpstan: Method WebServCo\Framework\SourceCode::__construct() has parameter $type with no typehint specified.">   public function __construct($type, $data)</warning></warning>
+<weak_warning descr="phpstan: Method WebServCo\Framework\SourceCode::__construct() has parameter $data with no typehint specified."><weak_warning descr="phpstan: Method WebServCo\Framework\SourceCode::__construct() has parameter $type with no typehint specified.">   public function __construct($type, $data)</weak_warning></weak_warning>
     {
         switch ($type) {
             case self::TYPE_XML:
                 break;
             default:
                 throw new \WebServCo\Framework\Exceptions\ApplicationException('Type not implemented.');
-<warning descr="phpstan: Unreachable statement - code above always terminates.">               break;</warning>
+<weak_warning descr="phpstan: Unreachable statement - code above always terminates.">               break;</weak_warning>
         }
         $this->type = $type;
         $this->data = $data;
     }
 
-<warning descr="phpstan: Method WebServCo\Framework\SourceCode::highlight() has no return typehint specified.">   public function highlight()</warning>
+<weak_warning descr="phpstan: Method WebServCo\Framework\SourceCode::highlight() has no return typehint specified.">   public function highlight()</weak_warning>
     {
         switch ($this->type) {
             case self::TYPE_XML:
                 return $this->highlightXml($this->data);
-<warning descr="phpstan: Unreachable statement - code above always terminates.">               break;</warning>
+<weak_warning descr="phpstan: Unreachable statement - code above always terminates.">               break;</weak_warning>
             default:
                 return false;
-<warning descr="phpstan: Unreachable statement - code above always terminates.">               break;</warning>
+<weak_warning descr="phpstan: Unreachable statement - code above always terminates.">               break;</weak_warning>
         }
     }
 
-<warning descr="phpstan: Method WebServCo\Framework\SourceCode::highlightXml() has no return typehint specified."><warning descr="phpstan: Method WebServCo\Framework\SourceCode::highlightXml() has parameter $data with no typehint specified.">   protected function highlightXml($data)</warning></warning>
+<weak_warning descr="phpstan: Method WebServCo\Framework\SourceCode::highlightXml() has no return typehint specified."><weak_warning descr="phpstan: Method WebServCo\Framework\SourceCode::highlightXml() has parameter $data with no typehint specified.">   protected function highlightXml($data)</weak_warning></weak_warning>
     {
         $data = htmlentities($data);
         $data = str_replace('&lt;', '<span style="color: purple">&lt;', $data);
@@ -41,4 +41,4 @@ final class SourceCode
         return $data;
     }
 }
-<warning descr="phpstan: Call to function count() on a separate line has no effect.">\count();</warning>
+<weak_warning descr="phpstan: Call to function count() on a separate line has no effect.">\count();</weak_warning>
