aecead9e4186e91b89a522fb7f8231bda48edeb8
Kirill Smelov <kirill.smelov@jetbrains.com>
2022-03-16 17:52:56 +0100

    WI-65764 Parse phpstan-return as return tag
    +correct parsing for throws tags
    
    GitOrigin-RevId: 24b174e3780b064859ffe0c30130471692b8e4ea

---
+++ src/com/jetbrains/php/phpstan/lang/documentation/parser/PhpStanDocThrowsParser.java
@@ -0,0 +1,6 @@
+package com.jetbrains.php.phpstan.lang.documentation.parser;
+
+import com.jetbrains.php.lang.documentation.phpdoc.parser.tags.PhpDocThrowsTagParser;
+
+public class PhpStanDocThrowsParser extends PhpDocThrowsTagParser {
+}
