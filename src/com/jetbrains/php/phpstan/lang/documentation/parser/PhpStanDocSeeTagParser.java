6704c8623828545b4e26110f73fc88daad2136c6
Kirill Smelov <kirill.smelov@jetbrains.com>
2022-04-28 14:00:53 +0200

    WI-65794 Parse `@phpstan-use` as extending doc tag
    GitOrigin-RevId: 17cd26a4b4934d89fb47bcaff8229133afa6ab9d

--- src/com/jetbrains/php/phpstan/lang/documentation/parser/PhpStanDocSeeTagParser.java
+++
@@ -1,6 +0,0 @@
-package com.jetbrains.php.phpstan.lang.documentation.parser;
-
-import com.jetbrains.php.lang.documentation.phpdoc.parser.tags.PhpDocSeeTagParser;
-
-public class PhpStanDocSeeTagParser extends PhpDocSeeTagParser {
-}
