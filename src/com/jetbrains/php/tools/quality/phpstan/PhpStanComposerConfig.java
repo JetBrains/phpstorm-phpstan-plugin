b746a271727170b5b78f3e1047bf794a1d06d210
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2022-05-09 15:22:50 +0200

    WI-54299 phpstan: parse memory limit from composer.json and update related field in inspection settings
    GitOrigin-RevId: 4dd22e7ae16c41691cc98d74012bb001be784b3c

--- src/com/jetbrains/php/tools/quality/phpstan/PhpStanComposerConfig.java
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanComposerConfig.java
@@ -1,6 +1,8 @@
 package com.jetbrains.php.tools.quality.phpstan;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.NlsSafe;
@@ -21,9 +23,11 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
 import java.util.List;
 
 import static com.intellij.openapi.util.text.StringUtil.*;
+import static com.jetbrains.php.composer.ComposerConfigUtils.parseJson;
 import static com.jetbrains.php.tools.quality.phpstan.PhpStanOpenSettingsProvider.PHP_STAN_OPEN_SETTINGS_PROVIDER;
 
 public class PhpStanComposerConfig extends QualityToolsComposerConfig<PhpStanConfiguration, PhpStanValidationInspection> {
@@ -57,7 +61,50 @@ public class PhpStanComposerConfig extends QualityToolsComposerConfig<PhpStanCon
     if (customRulesetFile != null) {
       return modifyRulesetPhpStanInspectionSetting(project, tool -> applyRuleset(tool, customRulesetFile.getPath()));
     }
+    final String memoryLimit = getMemoryLimit(config);
+    if (memoryLimit != null) {
+      return modifyRulesetPhpStanInspectionSetting(project, tool -> applyMemoryLimit(tool, memoryLimit));
+    }
     return false;
+  }
+
+  private String getMemoryLimit(VirtualFile config) {
+    JsonElement element;
+    try {
+      element = parseJson(config);
+    }
+    catch (IOException | JsonParseException e) {
+      return null;
+    }
+
+    if (element instanceof JsonObject) {
+      final JsonElement scriptElement = ((JsonObject)element).get("scripts");
+      if (scriptElement != null) {
+        final Ref<String> result = new Ref<>();
+        parse(scriptElement, result, (el, res) -> parseLimit(el, result));
+        return result.isNull() ? null : result.get();
+      }
+    }
+    return null;
+  }
+
+  private static void parseLimit(JsonElement el, Ref<String> result) {
+    final String string = el.getAsString();
+    if (string != null && string.contains("phpstan")) {
+      final List<String> split = split(string, " ");
+      for (String arg: split) {
+        final String prefix = "--memory-limit=";
+        if (startsWith(arg, prefix)) {
+          result.set(trimStart(arg, prefix));
+          return;
+        }
+        final int index = split.indexOf(arg);
+        if (StringUtil.equals(arg, "--memory-limit") && index < split.size() - 1) {
+          result.set(split.get(index + 1));
+          return;
+        }
+      }
+    }
   }
 
   @Override
@@ -101,6 +148,9 @@ public class PhpStanComposerConfig extends QualityToolsComposerConfig<PhpStanCon
 
   private static void applyRuleset(PhpStanGlobalInspection tool, @NlsSafe String customRuleset) {
     tool.config = customRuleset;
+  }
+  private static void applyMemoryLimit(PhpStanGlobalInspection tool, @NlsSafe String memoryLimit) {
+    tool.memoryLimit = memoryLimit;
   }
 
   protected boolean modifyRulesetPhpStanInspectionSetting(@NotNull Project project, @NotNull Consumer<PhpStanGlobalInspection> consumer) {
