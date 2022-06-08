530e863edef29775919d285f088c5a35d782d331
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2022-06-08 15:56:26 +0200

    WI-44541 Add an option to apply PHP CS Fixer rules to Reformat File Dialog -
    move options from inspection to tool settings
    
    GitOrigin-RevId: 2f3a01a8f14bc54c3e9f7b0a5bc8fdbe87176fc1

--- src/com/jetbrains/php/tools/quality/phpstan/PhpStanConfigurable.java
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanConfigurable.java
@@ -3,14 +3,19 @@ package com.jetbrains.php.tools.quality.phpstan;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.project.Project;
 import com.jetbrains.php.tools.quality.QualityToolProjectConfigurableForm;
-import com.jetbrains.php.tools.quality.QualityToolProjectConfiguration;
 import com.jetbrains.php.tools.quality.QualityToolType;
+import com.jetbrains.php.tools.quality.QualityToolsOptionsPanel;
 import org.jetbrains.annotations.NotNull;
 
 public class PhpStanConfigurable extends QualityToolProjectConfigurableForm implements Configurable.NoScroll {
 
   public PhpStanConfigurable(@NotNull Project project) {
     super(project);
+  }
+
+  @Override
+  protected QualityToolsOptionsPanel getQualityToolOptionPanel() {
+    return new PhpStanOptionsPanel(new PhpStanGlobalInspection());
   }
 
   @Override
