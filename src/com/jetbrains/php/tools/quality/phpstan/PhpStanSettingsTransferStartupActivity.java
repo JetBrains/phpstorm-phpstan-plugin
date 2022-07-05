202e2a7d569da7ef31fa6566284f3f9a418bbd32
Olga Strizhenko <olga.strizhenko@jetbrains.com>
2022-07-05 10:56:53 +0200

    WI-44541 don't transfer setting in tests and headless mode
    GitOrigin-RevId: 56240d994d7b9a0aa0820a6ff208e33e9b14c256

--- src/com/jetbrains/php/tools/quality/phpstan/PhpStanSettingsTransferStartupActivity.java
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanSettingsTransferStartupActivity.java
@@ -1,5 +1,7 @@
 package com.jetbrains.php.tools.quality.phpstan;
 
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.profile.codeInspection.InspectionProfileManager;
@@ -9,6 +11,10 @@ public class PhpStanSettingsTransferStartupActivity implements StartupActivity {
   @Override
   public void runActivity(@NotNull Project project) {
     if (project.isDefault()) {
+      return;
+    }
+    Application app = ApplicationManager.getApplication();
+    if (app.isUnitTestMode() || app.isHeadlessEnvironment()) {
       return;
     }
 
