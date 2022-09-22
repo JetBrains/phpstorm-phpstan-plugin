96033d2099d7f1a3198bb093511daf47dbe0e6a8
Filippova Maria <maria.filippova@jetbrains.com>
2022-09-22 21:29:45 +0200

    WI-66465 Support parsing of PhpStan's conditional return types
    GitOrigin-RevId: 61ad544d71445e80b3eb3d49cfc24fdfe26842b4

---
+++ testData/codeInsight/typeInference/NestedConditionalTypes.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @return ($a is int|bool ? ($b is string ? int : string) : ($c is positive-int ? array : bool))
+ */
+function a($a, $b, $c)
+{
+
+}
+<type value="int|array|string|bool">a(0, 1, 2)</type>;
