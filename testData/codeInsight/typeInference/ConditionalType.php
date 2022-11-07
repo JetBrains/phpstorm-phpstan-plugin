3aece4cd19423993b5848281503f9d6c5eca0cd3
Filippova Maria <maria.filippova@jetbrains.com>
2022-11-07 23:12:48 +0100

    WI-69578 PhpStan conditional types: support static/self
    GitOrigin-RevId: 1df882a528a8ff813964a16cc518b35028222cb6

--- testData/codeInsight/typeInference/ConditionalType.php
+++ testData/codeInsight/typeInference/ConditionalType.php
@@ -14,3 +14,16 @@ class is{}
 function f(int $size){}
 
 <type value="is">f()</type>;
+
+class ConditionalThis
+{
+    /**
+     * @return (static is int ? int : string)
+     */
+    public function foo(): int|string
+    {
+
+    }
+}
+
+<type value="int|string">(new ConditionalThis())->foo()</type>
