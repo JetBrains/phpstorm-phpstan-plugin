f140b5a9122d8799a19f4169561aba8d52bb8518
Maria Filippova <maria.filippova@jetbrains.com>
2022-11-29 11:48:57 +0100

    WI-69942 Render PHPDoc: render '@phpstan-template-covariant' properly
    GitOrigin-RevId: 9214f4c54e666d0fb6e3a3cf9b355ce4b9106aee

--- testData/codeInsight/documentationProvider/PhpStanTags.php
+++ testData/codeInsight/documentationProvider/PhpStanTags.php
@@ -6,6 +6,7 @@
  * @phpstan-param $b array<int, Foo>
  * @phpstan-throws Exception
  * @phpstan-template T of Foo with description
+ * @phpstan-template-covariant M of Foo with description
  */
 function fo<caret>o(int $a, $b) {
 
