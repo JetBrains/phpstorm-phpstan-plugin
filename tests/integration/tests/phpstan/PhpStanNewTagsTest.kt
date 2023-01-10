04b7d0510d389d29d890244c2a6c7848462ad3b0
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2023-01-10 21:21:14 +0100

    WI-70706 PhpStan/Psalm: add test which will fail when new tags appear
    Add actual check whether there is a difference with previous tags
    
    GitOrigin-RevId: 9bd8bd2f5d32b068ef5734e678a64ca48c740177

--- tests/integration/tests/phpstan/PhpStanNewTagsTest.kt
+++ tests/integration/tests/phpstan/PhpStanNewTagsTest.kt
@@ -3,6 +3,7 @@ package integration.tests.phpstan
 import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.psi.util.childrenOfType
+import com.intellij.testFramework.UsefulTestCase
 import com.jetbrains.php.PhpIndex
 import com.jetbrains.php.fixtures.PhpCodeInsightFixtureTestCase
 import com.jetbrains.php.lang.psi.elements.impl.ArrayCreationExpressionImpl
@@ -14,6 +15,7 @@ import java.io.File
 import java.io.FileOutputStream
 import java.net.URL
 import java.util.zip.ZipInputStream
+import kotlin.io.path.div
 
 class PhpStanNewTagsTest : PhpCodeInsightFixtureTestCase() {
 
@@ -37,7 +39,7 @@ class PhpStanNewTagsTest : PhpCodeInsightFixtureTestCase() {
 
   override fun tearDown() {
     try {
-      FileUtil.deleteRecursively(phpstanFolder.toPath())
+      //FileUtil.deleteRecursively(phpstanFolder.toPath())
     }
     catch (e: Throwable) {
       addSuppressedException(e)
@@ -60,9 +62,16 @@ class PhpStanNewTagsTest : PhpCodeInsightFixtureTestCase() {
         }
       }
     }
-    val tagsFile = FileUtil.createTempFile("phpstan-tags.txt", "")
-    tagsFile.writeText(annotations.joinToString("\n"))
-    println("##teamcity[publishArtifacts '${tagsFile.absolutePath}']")
+    val resultFileName = "phpstan-tags.txt"
+    val resultFile = FileUtil.createTempFile(resultFileName, "")
+    resultFile.writeText(annotations.joinToString("\n"))
+    println("##teamcity[publishArtifacts '${resultFile.absolutePath}']")
+    val pathToPreviousResults = (phpstanFolder.toPath() / "previousResults" / resultFileName).toFile()
+    if(pathToPreviousResults.exists()){
+      UsefulTestCase.assertSameElements(annotations, pathToPreviousResults.readLines())
+    } else {
+      println("Previous results are not found at ${pathToPreviousResults.absolutePath}")
+    }
   }
   private fun downloadAndUnpackZip(zipUrl: URL, destinationDirectory: File) {
     val zipConnection = zipUrl.openConnection()
