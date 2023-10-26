d29d4339e110e6705ce64fdbd83a710b66bfdc6d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-10-26 12:41:36 +0200

    IJPL-310 mark extensions as obsolete (part 11)
    GitOrigin-RevId: ef915fa2bf11f9fbcdf9ebbf80bc553129314335

--- src/com/jetbrains/php/tools/quality/phpstan/PhpStanConfigurationProvider.java
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanConfigurationProvider.java
@@ -2,11 +2,10 @@ package com.jetbrains.php.tools.quality.phpstan;
 
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.extensions.ExtensionPointName;
-import com.intellij.openapi.project.Project;
-import com.intellij.util.NullableFunction;
 import com.jetbrains.php.tools.quality.QualityToolConfigurationProvider;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public abstract class PhpStanConfigurationProvider extends QualityToolConfigurationProvider<PhpStanConfiguration> {
   private static final Logger LOG = Logger.getInstance(PhpStanConfigurationProvider.class);
@@ -15,10 +14,10 @@ public abstract class PhpStanConfigurationProvider extends QualityToolConfigurat
 
   @Nullable
   public static PhpStanConfigurationProvider getInstances() {
-    final PhpStanConfigurationProvider[] extensions = EP_NAME.getExtensions();
-    if (extensions.length > 1) {
+    List<PhpStanConfigurationProvider> extensions = EP_NAME.getExtensionList();
+    if (extensions.size() > 1) {
       LOG.error("Several providers for remote PHPStan configuration was found");
     }
-    return extensions.length == 0 ? null : extensions[0];
+    return extensions.isEmpty() ? null : extensions.get(0);
   }
 }
