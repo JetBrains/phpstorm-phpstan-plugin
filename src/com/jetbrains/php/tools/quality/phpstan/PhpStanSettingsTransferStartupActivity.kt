054f6ca007dcc8e1df8ff29fd223e367a029a29b
Artemy Pestretsov <artemy.pestretsov@jetbrains.com>
2024-01-24 14:11:46 +0100

    [php] refactor: migrate to ProjectActivity
    GitOrigin-RevId: 452547d800bd3ffb74dbf72a41d264ff5a6d9547

--- src/com/jetbrains/php/tools/quality/phpstan/PhpStanSettingsTransferStartupActivity.kt
+++ src/com/jetbrains/php/tools/quality/phpstan/PhpStanSettingsTransferStartupActivity.kt
@@ -1,35 +1,28 @@
-package com.jetbrains.php.tools.quality.phpstan;
+package com.jetbrains.php.tools.quality.phpstan
 
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.StartupActivity;
-import com.intellij.profile.codeInspection.InspectionProfileManager;
-import com.intellij.util.PlatformUtils;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.ProjectActivity
+import com.intellij.profile.codeInspection.InspectionProfileManager
+import com.intellij.util.PlatformUtils
 
-public class PhpStanSettingsTransferStartupActivity implements StartupActivity {
-  @Override
-  public void runActivity(@NotNull Project project) {
-    if (project.isDefault()) {
-      return;
-    }
-    Application app = ApplicationManager.getApplication();
-    if (app.isUnitTestMode() || app.isHeadlessEnvironment() || !PlatformUtils.isPhpStorm()) {
-      return;
-    }
+class PhpStanSettingsTransferStartupActivity : ProjectActivity {
+  override suspend fun execute(project: Project) {
+    if (project.isDefault) return
+    val app = ApplicationManager.getApplication()
+    if (app.isUnitTestMode || app.isHeadlessEnvironment || !PlatformUtils.isPhpStorm()) return
 
-    PhpStanGlobalInspection tool =
-      (PhpStanGlobalInspection)PhpStanQualityToolType.INSTANCE.getGlobalTool(project,
-                                                                         InspectionProfileManager.getInstance(project).getCurrentProfile());
-    PhpStanOptionsConfiguration instance = PhpStanOptionsConfiguration.getInstance(project);
-    if (tool != null && !instance.isTransferred()) {
-      instance.setConfig(tool.config);
-      instance.setAutoload(tool.autoload);
-      instance.setLevel(tool.level);
-      instance.setMemoryLimit(tool.memoryLimit);
-      instance.setFullProject(tool.FULL_PROJECT);
-      instance.setTransferred(true);
+    val tool = PhpStanQualityToolType.INSTANCE.getGlobalTool(project, InspectionProfileManager.getInstance(project).currentProfile) as? PhpStanGlobalInspection
+    val instance = PhpStanOptionsConfiguration.getInstance(project)
+    tool?.let {
+      if (!instance.isTransferred) {
+        instance.config = tool.config
+        instance.autoload = tool.autoload
+        instance.level = tool.level
+        instance.memoryLimit = tool.memoryLimit
+        instance.isFullProject = tool.FULL_PROJECT
+        instance.isTransferred = true
+      }
     }
   }
 }
