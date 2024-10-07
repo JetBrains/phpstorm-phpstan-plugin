a97c443549919d3014b6beb349fd9146cefc7018
Artemy Pestretsov <artemy.pestretsov@jetbrains.com>
2024-10-07 14:12:59 +0200

    WI-74237 groovy -> java leftovers
    GitOrigin-RevId: c7790b4006074b8e147a3fda37d549f4d4ba2063

--- tests/com/jetbrains/php/phpstan/quality/tools/PhpStanConfigFileFromComposerTest.java
+++ tests/com/jetbrains/php/phpstan/quality/tools/PhpStanConfigFileFromComposerTest.java
@@ -1,79 +1,77 @@
-package com.jetbrains.php.phpstan.quality.tools
+package com.jetbrains.php.phpstan.quality.tools;
 
-import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase
-import com.jetbrains.php.tools.quality.phpstan.PhpStanComposerConfig
-import org.jetbrains.annotations.NotNull
-import org.jetbrains.annotations.Nullable
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
+import com.jetbrains.php.tools.quality.phpstan.PhpStanComposerConfig;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-class PhpStanConfigFileFromComposerTest extends CodeInsightFixtureTestCase {
-
-  @NotNull
+public class PhpStanConfigFileFromComposerTest extends CodeInsightFixtureTestCase {
   private void doRulesetTest(@NotNull String content, @Nullable String expectedPath) {
-    myFixture.configureByText("composer.json", content)
-    try {
-      def configFile = myFixture.getFile().getVirtualFile()
-      assertEquals(expectedPath, new PhpStanComposerConfig().getRuleset(configFile))
-    }
-    finally {
-    }
+    myFixture.configureByText("composer.json", content);
+    VirtualFile configFile = myFixture.getFile().getVirtualFile();
+    assertEquals(expectedPath, new PhpStanComposerConfig().getRuleset(configFile));
   }
 
-  void testCFormat() {
-    doRulesetTest('''
-{
-    "scripts": {
-        "phpstan": "vendor/bin/phpstan analyse -l 2 -c tests/phpstan/phpstan.neon system/src --memory-limit=256M"
-    }
-}
-''', "tests/phpstan/phpstan.neon")
+  public void testCFormat() {
+    doRulesetTest("""
+                    
+                    {
+                        "scripts": {
+                            "phpstan": "vendor/bin/phpstan analyse -l 2 -c tests/phpstan/phpstan.neon system/src --memory-limit=256M"
+                        }
+                    }
+                    """, "tests/phpstan/phpstan.neon");
   }
 
-  void testNoScriptsSection() {
-    doRulesetTest('''
-{
-}
-''', null)
+  public void testNoScriptsSection() {
+    doRulesetTest("""
+                    
+                    {
+                    }
+                    """, null);
   }
 
-  void testNoPHPStanInScriptsSection() {
-    doRulesetTest('''
-{
-    "scripts": {
-    }
-}
-''', null)
-  }
-  
-
-  void testConfigFormat() {
-    doRulesetTest('''
-{
-  "scripts": {
-        "phpstan": "vendor/bin/phpstan analyse -l 2 --configuration=tests/phpstan/phpstan.neon system/src --memory-limit=256M"
-  }
-}
-''', "tests/phpstan/phpstan.neon")
+  public void testNoPHPStanInScriptsSection() {
+    doRulesetTest("""
+                    
+                    {
+                        "scripts": {
+                        }
+                    }
+                    """, null);
   }
 
-  void testNoConfigArg() {
-    doRulesetTest('''
-{
-  "scripts": {
-        "phpstan": "vendor/bin/phpstan analyse -l 2 system/src --memory-limit=256M"
-  }
-}
-''', null)
-  }
-
-
-  void testWrongFormat() {
-    doRulesetTest('''
-{
-"scripts": {
-        "phpstan": "vendor/bin/phpstan analyse -l 2 -c"
-  }
-}
-''', null)
+  public void testConfigFormat() {
+    doRulesetTest("""
+                    
+                    {
+                      "scripts": {
+                            "phpstan": "vendor/bin/phpstan analyse -l 2 --configuration=tests/phpstan/phpstan.neon system/src --memory-limit=256M"
+                      }
+                    }
+                    """, "tests/phpstan/phpstan.neon");
   }
 
+  public void testNoConfigArg() {
+    doRulesetTest("""
+                    
+                    {
+                      "scripts": {
+                            "phpstan": "vendor/bin/phpstan analyse -l 2 system/src --memory-limit=256M"
+                      }
+                    }
+                    """, null);
+  }
+
+  public void testWrongFormat() {
+    doRulesetTest("""
+                    
+                    {
+                    "scripts": {
+                            "phpstan": "vendor/bin/phpstan analyse -l 2 -c"
+                      }
+                    }
+                    """, null);
+  }
 }
